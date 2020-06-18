18ab19b0465d233f44d9ee2a17eded8c71bd4eb8
IPostYellow <32611516+IPostYellow@users.noreply.github.com>
2020-06-18 16:29:40 +0800

    Delete 变态跳台阶.py

--- 变态跳台阶.py
+++
@@ -1,8 +0,0 @@
-#直接找规律 32 ms 5840K
-class Solution:
-    def jumpFloorII(self, number):
-        # write code here
-        result=1
-        for i in range(number-1):
-            result*=2
-        return result
