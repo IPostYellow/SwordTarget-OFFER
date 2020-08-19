d91fb2edcf15c6f8ecbd506c15c86d972bc86a9d
IPostYellow <32611516+IPostYellow@users.noreply.github.com>
2020-08-19 15:44:58 +0800

    Update 字符串的排列.py

--- 字符串的排列.py
+++ 字符串的排列.py
@@ -1,3 +1,8 @@
+'''
+输入一个字符串,
+按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,
+则按字典序打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+'''
 class Solution:#递归法 20ms,5752k
     def perm(self,pos,ss,ret):
         ss=list(ss)
